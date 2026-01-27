e3cba66123340a25c800b686ed6087becc0977a3
Costantino Dionis Gwaka <gwaka94@gmail.com>
2026-01-27 16:51:39 +0300

    fix: sets

---
+++ helloworld/setz.py
@@ -0,0 +1,4 @@
+# collection with no duplicate items
+numbers = {1, 2, 3, 4, 5, 1, 2}
+unique_numbers = set(numbers)
+print(unique_numbers)
---
+++ helloworld/tuples.py
@@ -0,0 +1,9 @@
+point = 1, 2
+print(type(point))
+
+points = tuple([1, 2])
+print(points)
+
+name = "Hello World"
+tuple_name = tuple(name)
+print(tuple_name)
