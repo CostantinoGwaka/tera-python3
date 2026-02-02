6824c352fa783ce107f0ae6ee55f2d101098f81e
Costantino Dionis Gwaka <gwaka94@gmail.com>
2026-02-02 09:43:20 +0300

    fix

--- helloworld/list.py
+++ helloworld/list.py
@@ -1,12 +1,28 @@
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 matrix = [[1, 2], [3, 4]]
-zeros = [0] * 10
+zeros = [0] * 100
 combined = letters + zeros
-print(combined)
+# print(combined)
 
 
 numbers = list(range(20))
-print(numbers)
+# print(numbers)
+print(numbers[::-1])
 
-chars = list("Hello World")
-print(chars)
+
+# chars = list("Hello World")
+# print(chars)
+
+
+# unpacking
+number = [1, 2, 3]
+# first = number[0]
+# second = number[1]
+# third = number[2]
+first, second, third = number
+print(first, second, third)
+
+
+numbers = [1, 2, 3, 4, 5]
+first, second, *rest = numbers
+print(first, second, rest)
--- helloworld/setz.py
+++ helloworld/setz.py
@@ -2,3 +2,11 @@
 numbers = {1, 2, 3, 4, 5, 1, 2}
 unique_numbers = set(numbers)
 print(unique_numbers)
+
+first_set = {1, 2, 3}
+second_set = {3, 4, 5}
+
+print(first_set | second_set)  # Union
+print(first_set & second_set)  # Intersection
+print(first_set - second_set)  # Difference
+print(first_set ^ second_set)  # Symmetric Difference
