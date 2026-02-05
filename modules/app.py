ba58e00500963039edd3b76e4b4f592f7b050409
Costantino Dionis Gwaka <gwaka94@gmail.com>
2026-02-05 14:01:16 +0300

    fix packages

---
+++ modules/app.py
@@ -0,0 +1,16 @@
+# from sales import cal_tax, cal_shipping
+# from sales import * -- import everything from sales
+
+
+# as import sales as s
+import sales
+import sys
+sales.cal_shipping()
+sales.cal_tax()
+
+
+print(sys.path)
+
+
+# cal_shipping()
+# cal_tax()
