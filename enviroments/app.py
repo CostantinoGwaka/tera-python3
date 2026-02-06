79649c8dba385b3fd6c79d1a2272814a10caf18d
Costantino Dionis Gwaka <gwaka94@gmail.com>
2026-02-06 16:24:48 +0300

    fix

---
+++ enviroments/app.py
@@ -0,0 +1,6 @@
+
+
+# python3 -m venv env -- create virtual environment
+# source env/bin/activate -- activate virtual environment
+# deactivate -- deactivate virtual environment
+# pip install requests -- install requests library
